76a98f34cdca6051fb60d3775d38355901be918d
Manan Mehra <mananmehra330@gmail.com>
2024-01-09 23:22:32 +0530

    Registration page added

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,12 +1,68 @@
+// Register.js
 import React from 'react';
-// import styles from './Register.module.css';
+import styles from './Register.module.css';
 
 const Register = () => {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <h1 className={styles['main-heading']}> REGISTRATION </h1>
+      <h2 className={styles['main-heading-2']}> REGISTRATION </h2>
+      <form className={styles['form']} action=''>
+
+        <div>
+          <label className={styles['sub-heading-1']} htmlFor="captainname">Captain’s Name</label>
+          <br />
+          <input className={`${styles['input-box']} ${styles['input-box-1']}`} type='text' name='captainname' id='captainname'/>
+        </div>
+        <br />
+
+        <div>
+          <label className={styles['sub-heading-2']} htmlFor="captaincontact">Captain’s Contact Number</label>
+          <br />
+          <input className={`${styles['input-box']} ${styles['input-box-2']}`} type='text' name='captaincontact' id='captaincontact'/>
+        </div>
+        <br />
+
+        <div>
+          <label className={styles['sub-heading-3']} htmlFor="captainroll">Captain’s Roll No. </label>
+          <br></br>
+          <input className={`${styles['input-box']} ${styles['input-box-3']}`} type='text' name='captainroll' id='captainroll'/>
+        </div>
+        <br />
+
+        <div>
+          <label className={styles['sub-heading-4']} htmlFor="sport">Name of Sports</label>
+          <br />
+          <input className={`${styles['input-box']} ${styles['input-box-4']}`} type='text' name='sport' id='sport'/>
+        </div>
+        <br />
+
+        <div>
+          <label className={styles['sub-heading-5']} htmlFor="collegename">College Name</label>
+          <br />
+          <input className={`${styles['input-box']} ${styles['input-box-5']}`} type='text' name='collegename' id='collegename'/>
+        </div>
+        <br />
+
+        <div>
+          <label className={styles['sub-heading-6']} htmlFor="numberofplayers">Number of players in a Team</label>
+          <br />
+          <input className={`${styles['input-box']} ${styles['input-box-6']}`} type='text' name='numberofplayers' id='numberofplayers'/>
+        </div>
+        <br />
+
+        <div>
+          <label className={styles['sub-heading-7']} htmlFor="playersandcontact">Number of players and Contact number</label>
+          <br />
+          <input className={`${styles['input-box']} ${styles['input-box-7']}`} type='text' name='playersandcontact' id='playersandcontact'/>
+        </div>
+        <br />
+
+        <button type='submit' className={styles['submit-button']}>Submit</button>
+ 
+      </form>
+    </>
+  );
 }
 
-export default Register
+export default Register;
