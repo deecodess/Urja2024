53cccd62037d940aef0a45c75d09db68fd456fe1
ARYAN DOGRA <dograwarrior2003@gmail.com>
2024-01-10 15:20:07 +0530

    gallery page completed

--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { Splide, SplideSlide, SplideTrack } from "@splidejs/react-splide";
-import { GenerateSlides } from "./GenerateSlides";
+import {
+  GenerateSlides1,
+  GenerateSlides2,
+  GenerateSlides3,
+  GenerateSlides4,
+} from "./GenerateSlides";
 import classes from "./Gallery.module.css";
 import { Helmet } from "react-helmet";
 // Default theme
@@ -17,13 +22,39 @@ const AutoplayExample = () => {
   const options = {
     type: "loop",
     gap: "2rem",
-    autoplay: true,
+    autoplay: false,
     pauseOnHover: false,
     resetProgress: false,
-    height: "15rem",
-    perPage: 3,
-    perMove: 1,
+    pagination: false,
     padding: "4rem",
+    drag: true,
+    breakpoints: {
+      640: {
+        perPage: 1,
+        width: "100%", // Set the width for screens with width less than 640px
+        height: "10rem", // Set the height for screens with width less than 640px
+      },
+      768: {
+        perPage: 2,
+        width: "100%", // Set the width for screens with width between 640px and 767px
+        height: "12rem", // Set the height for screens with width between 640px and 767px
+      },
+      1024: {
+        perPage: 3,
+        width: "100%", // Set the width for screens with width between 768px and 1023px
+        height: "15rem", // Set the height for screens with width between 768px and 1023px
+      },
+      1440: {
+        perPage: 4,
+        width: "100%", // Set the width for screens with width between 1024px and 1439px
+        height: "18rem", // Set the height for screens with width between 1024px and 1439px
+      },
+      2560: {
+        perPage: 4,
+        width: "100%", // Set the width for screens with width between 1024px and 1439px
+        height: "18rem", // Set the height for screens with width between 1024px and 1439px
+      },
+    },
   };
 
   return (
@@ -31,24 +62,21 @@ const AutoplayExample = () => {
       <Helmet>
         <title>URJA | Gallery</title>
       </Helmet>
-      <div
-        style={{
-          backgroundColor: "#171717",
-          maxHeight: "-webkit-fit-content",
-          padding: "1rem",
-        }}
-      >
-        <div>
-          <h1 className={classes.heading}>GALLERY</h1>
-          <hr style={{ maxWidth: "25rem", backgroundColor: "#E8E8E8" }} />
-          <h4
-            style={{
-              textAlign: "center",
-              color: "white",
-              fontFamily: "'Museo Sans Rounded', sans-serif",
-              fontWeight: 400,
-            }}
-          >
+      <div className={classes.container}>
+        <div className={classes.galleryContainer}>
+          <div style={{ position: "relative" }}>
+            <div className={classes.bgtext}></div>
+            <h1 className={classes.heading}>GALLERY</h1>
+            <hr
+              style={{
+                maxWidth: "28rem",
+                zIndex: 2,
+                backgroundColor: "#E8E8E8",
+              }}
+            />
+          </div>
+
+          <h4 className={classes.customh4}>
             Experience the thrill and excitement of our sports fest through our
             stunning Photo Gallery. The Action Shots...
           </h4>
@@ -56,31 +84,55 @@ const AutoplayExample = () => {
             options={options}
             aria-labelledby="autoplay-example-heading"
             hasTrack={false}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100vh", // Ensure the container takes the full height of the viewport
+            }}
           >
-            <div style={{ position: "relative", objectFit: "contain" }}>
+            <div
+              style={{
+                position: "relative",
+                objectFit: "contain",
+                textAlign: "center",
+              }}
+            >
               <SplideTrack>
-                {GenerateSlides().map((slide) => (
+                {GenerateSlides1().map((slide) => (
                   <SplideSlide
                     key={slide.src}
                     data-splide-interval="1050"
                     style={{
                       backgroundColor: "#232323",
                       textAlign: "center",
-                      borderRadius: "1.1rem",
-                      objectFit: "fill",
+                      borderRadius: "8px",
+                      objectFit: "contain",
+                      maxWidth: "80vw",
+                      maxHeight: "60vh",
                     }}
                   >
-                    <img
-                      src={slide.src}
-                      alt={slide.alt}
+                    <div
                       style={{
-                        maxWidth: "80%",
-                        margin: "0.8rem",
-                        borderRadius: "1.1rem",
-                        maxHeight: "20rem",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        height: "80%", // Ensure the container takes the full height
                       }}
-                      loading="lazy"
-                    />
+                    >
+                      <img
+                        src={slide.src}
+                        alt={slide.alt}
+                        style={{
+                          maxWidth: "80%", // Adjust the maximum width as needed
+                          maxHeight: "60vh", // Adjust the maximum height as needed
+                          width: "auto", // Ensure the image is responsive
+                          height: "auto", // Ensure the image is responsive
+                          borderRadius: "2px",
+                        }}
+                        loading="lazy"
+                      />
+                    </div>
                   </SplideSlide>
                 ))}
               </SplideTrack>
@@ -99,31 +151,55 @@ const AutoplayExample = () => {
             options={options}
             aria-labelledby="autoplay-example-heading"
             hasTrack={false}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100vh", // Ensure the container takes the full height of the viewport
+            }}
           >
-            <div style={{ position: "relative" }}>
+            <div
+              style={{
+                position: "relative",
+                objectFit: "contain",
+                textAlign: "center",
+              }}
+            >
               <SplideTrack>
-                {GenerateSlides().map((slide) => (
+                {GenerateSlides2().map((slide) => (
                   <SplideSlide
                     key={slide.src}
-                    data-splide-interval="1250"
+                    data-splide-interval="1050"
                     style={{
                       backgroundColor: "#232323",
                       textAlign: "center",
-                      borderRadius: "1.1rem",
-                      objectFit: "fill",
+                      borderRadius: "8px",
+                      objectFit: "contain",
+                      maxWidth: "80vw",
+                      maxHeight: "60vh",
                     }}
                   >
-                    <img
-                      src={slide.src}
-                      alt={slide.alt}
+                    <div
                       style={{
-                        maxWidth: "80%",
-                        margin: "0.8rem",
-                        borderRadius: "1.1rem",
-                        maxHeight: "20rem",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        height: "80%", // Ensure the container takes the full height
                       }}
-                      loading="lazy"
-                    />
+                    >
+                      <img
+                        src={slide.src}
+                        alt={slide.alt}
+                        style={{
+                          maxWidth: "80%", // Adjust the maximum width as needed
+                          maxHeight: "60vh", // Adjust the maximum height as needed
+                          width: "auto", // Ensure the image is responsive
+                          height: "auto", // Ensure the image is responsive
+                          borderRadius: "2px",
+                        }}
+                        loading="lazy"
+                      />
+                    </div>
                   </SplideSlide>
                 ))}
               </SplideTrack>
@@ -142,31 +218,55 @@ const AutoplayExample = () => {
             options={options}
             aria-labelledby="autoplay-example-heading"
             hasTrack={false}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100vh", // Ensure the container takes the full height of the viewport
+            }}
           >
-            <div style={{ position: "relative" }}>
+            <div
+              style={{
+                position: "relative",
+                objectFit: "contain",
+                textAlign: "center",
+              }}
+            >
               <SplideTrack>
-                {GenerateSlides().map((slide) => (
+                {GenerateSlides3().map((slide) => (
                   <SplideSlide
                     key={slide.src}
-                    data-splide-interval="1150"
+                    data-splide-interval="1050"
                     style={{
                       backgroundColor: "#232323",
                       textAlign: "center",
-                      borderRadius: "1.1rem",
-                      objectFit: "fill",
+                      borderRadius: "8px",
+                      objectFit: "contain",
+                      maxWidth: "80vw",
+                      maxHeight: "60vh",
                     }}
                   >
-                    <img
-                      src={slide.src}
-                      alt={slide.alt}
+                    <div
                       style={{
-                        maxWidth: "80%",
-                        margin: "0.8rem",
-                        borderRadius: "1.1rem",
-                        maxHeight: "20rem",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        height: "80%", // Ensure the container takes the full height
                       }}
-                      loading="lazy"
-                    />
+                    >
+                      <img
+                        src={slide.src}
+                        alt={slide.alt}
+                        style={{
+                          maxWidth: "80%", // Adjust the maximum width as needed
+                          maxHeight: "60vh", // Adjust the maximum height as needed
+                          width: "auto", // Ensure the image is responsive
+                          height: "auto", // Ensure the image is responsive
+                          borderRadius: "2px",
+                        }}
+                        loading="lazy"
+                      />
+                    </div>
                   </SplideSlide>
                 ))}
               </SplideTrack>
@@ -185,31 +285,55 @@ const AutoplayExample = () => {
             options={options}
             aria-labelledby="autoplay-example-heading"
             hasTrack={false}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100vh", // Ensure the container takes the full height of the viewport
+            }}
           >
-            <div style={{ position: "relative" }}>
+            <div
+              style={{
+                position: "relative",
+                objectFit: "contain",
+                textAlign: "center",
+              }}
+            >
               <SplideTrack>
-                {GenerateSlides().map((slide) => (
+                {GenerateSlides4().map((slide) => (
                   <SplideSlide
                     key={slide.src}
-                    data-splide-interval="1350"
+                    data-splide-interval="1050"
                     style={{
                       backgroundColor: "#232323",
                       textAlign: "center",
-                      borderRadius: "1.1rem",
-                      objectFit: "fill",
+                      borderRadius: "8px",
+                      objectFit: "contain",
+                      maxWidth: "80vw",
+                      maxHeight: "60vh",
                     }}
                   >
-                    <img
-                      src={slide.src}
-                      alt={slide.alt}
+                    <div
                       style={{
-                        maxWidth: "80%",
-                        margin: "0.8rem",
-                        borderRadius: "1.1rem",
-                        maxHeight: "20rem",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        height: "80%", // Ensure the container takes the full height
                       }}
-                      loading="lazy"
-                    />
+                    >
+                      <img
+                        src={slide.src}
+                        alt={slide.alt}
+                        style={{
+                          maxWidth: "80%", // Adjust the maximum width as needed
+                          maxHeight: "60vh", // Adjust the maximum height as needed
+                          width: "auto", // Ensure the image is responsive
+                          height: "auto", // Ensure the image is responsive
+                          borderRadius: "2px",
+                        }}
+                        loading="lazy"
+                      />
+                    </div>
                   </SplideSlide>
                 ))}
               </SplideTrack>
--- src/Pages/Gallery/GenerateSlides.jsx
+++ src/Pages/Gallery/GenerateSlides.jsx
@@ -1,4 +1,4 @@
-const imageLinks = [
+const imageLinks1 = [
   "https://drive.google.com/uc?export=view&id=1scxuWAu1A1OdlacmF2EFk0g9W-Mbyixk",
   "https://drive.google.com/uc?export=view&id=1ArV2XoVWdJW9tBaWqNaMg-yLojT1TGUr",
   "https://drive.google.com/uc?export=view&id=1_vMWflC3qdfEGVecTQOt7Y01KogfkH5U",
@@ -10,10 +10,71 @@ const imageLinks = [
   "https://drive.google.com/uc?export=view&id=10uE5KUWk-mxgqDTqSdFcqDg3SbGZRWrv",
   "https://drive.google.com/uc?export=view&id=1hefNeBxA_K1z044dhJK-9iFVsAk6J4QD",
 ];
-export function GenerateSlides(length = 10) {
+const imageLinks2 = [
+  "https://drive.google.com/uc?export=view&id=1Ybva9mKD004p5qtrRIGhpVEQJka4oqoW",
+  "https://drive.google.com/uc?export=view&id=1M5Z36fvJh8KT15pdGGmIU337LAioD2_G",
+  "https://drive.google.com/uc?export=view&id=1AyJoxTTyWjaYx_1qRoxIk5FVRD1Fijal",
+  "https://drive.google.com/uc?export=view&id=1pBqFCBWuPu74rntSQvaaqo9MArePX9Z-",
+  "https://drive.google.com/uc?export=view&id=1sSlCTiLZe9D_bZ8KXHdWlkz36jhmCUmI",
+  "https://drive.google.com/uc?export=view&id=1MebQJY2IwNw2MXHSayx5FicgersJYDCq",
+  "https://drive.google.com/uc?export=view&id=1b-HV9Z4pw4peqtriEd95jxVTX_xhPZTY",
+  "https://drive.google.com/uc?export=view&id=1-FXLVmWLM1x9SAiIIHfZA4uo5l6OYeMV",
+  "https://drive.google.com/uc?export=view&id=1NBd2OpnOfsRdZVhb6iZlJDIDNDONXVza",
+  "https://drive.google.com/uc?export=view&id=1b7EFfLSxW_PT5ExKuB8snxyIBzEceALQ",
+];
+const imageLinks3 = [
+  "https://drive.google.com/uc?export=view&id=1scxuWAu1A1OdlacmF2EFk0g9W-Mbyixk",
+  "https://drive.google.com/uc?export=view&id=1ArV2XoVWdJW9tBaWqNaMg-yLojT1TGUr",
+  "https://drive.google.com/uc?export=view&id=1_vMWflC3qdfEGVecTQOt7Y01KogfkH5U",
+  "https://drive.google.com/uc?export=view&id=1M4ELKoIW_RjVqu_ws562YIJEXGgQeyfG",
+  "https://drive.google.com/uc?export=view&id=1aKnRAkenDKkm0u564-iXPCJfv8hM9sDv",
+  "https://drive.google.com/uc?export=view&id=1OKQTK3yQ1tT_7cxTonfAQp1YmJT_g0d8",
+  "https://drive.google.com/uc?export=view&id=18cFs6bnYOA3Oi0CkuKwlQ1c30_THqFfW",
+  "https://drive.google.com/uc?export=view&id=1bC0FsXzxR1rLdn_ZGdj5JqiK5g6GJXIb",
+  "https://drive.google.com/uc?export=view&id=10uE5KUWk-mxgqDTqSdFcqDg3SbGZRWrv",
+  "https://drive.google.com/uc?export=view&id=1hefNeBxA_K1z044dhJK-9iFVsAk6J4QD",
+];
+const imageLinks4 = [
+  "https://drive.google.com/uc?export=view&id=1Ybva9mKD004p5qtrRIGhpVEQJka4oqoW",
+  "https://drive.google.com/uc?export=view&id=1M5Z36fvJh8KT15pdGGmIU337LAioD2_G",
+  "https://drive.google.com/uc?export=view&id=1AyJoxTTyWjaYx_1qRoxIk5FVRD1Fijal",
+  "https://drive.google.com/uc?export=view&id=1pBqFCBWuPu74rntSQvaaqo9MArePX9Z-",
+  "https://drive.google.com/uc?export=view&id=1sSlCTiLZe9D_bZ8KXHdWlkz36jhmCUmI",
+  "https://drive.google.com/uc?export=view&id=1MebQJY2IwNw2MXHSayx5FicgersJYDCq",
+  "https://drive.google.com/uc?export=view&id=1b-HV9Z4pw4peqtriEd95jxVTX_xhPZTY",
+  "https://drive.google.com/uc?export=view&id=1-FXLVmWLM1x9SAiIIHfZA4uo5l6OYeMV",
+  "https://drive.google.com/uc?export=view&id=1NBd2OpnOfsRdZVhb6iZlJDIDNDONXVza",
+  "https://drive.google.com/uc?export=view&id=1b7EFfLSxW_PT5ExKuB8snxyIBzEceALQ",
+];
+export function GenerateSlides1(length = 10) {
   return Array.from({ length }).map((value, index) => {
     return {
-      src: imageLinks[index % imageLinks.length],
+      src: imageLinks1[index % imageLinks1.length],
+      alt: `Image ${index + 1}`,
+    };
+  });
+}
+
+export function GenerateSlides2(length = 10) {
+  return Array.from({ length }).map((value, index) => {
+    return {
+      src: imageLinks2[index % imageLinks2.length],
+      alt: `Image ${index + 1}`,
+    };
+  });
+}
+export function GenerateSlides3(length = 10) {
+  return Array.from({ length }).map((value, index) => {
+    return {
+      src: imageLinks3[index % imageLinks3.length],
+      alt: `Image ${index + 1}`,
+    };
+  });
+}
+export function GenerateSlides4(length = 10) {
+  return Array.from({ length }).map((value, index) => {
+    return {
+      src: imageLinks4[index % imageLinks4.length],
       alt: `Image ${index + 1}`,
     };
   });
