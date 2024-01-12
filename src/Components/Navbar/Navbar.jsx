0969585cd86b129aa2ef617a3940f61d25d24ee3
kabir26goyal <124425784+kabir26goyal@users.noreply.github.com>
2024-01-12 18:27:35 +0530

    Update Navbar.jsx
    Added the navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,34 @@
-// Navbar still have to be added
 import React from 'react';
 import Urja_logo from '../../assets/Group 23.png';
 import Main from '../../assets/Group 19.png';
 import Lion_img from '../../assets/Firefly a golden lion head trophy on a long pillar 12534 2.png';
-import Roar from '../../assets/Group 21 (1).png'
-import Urja_Bg from '../../assets/URJA.png'
+import Roar from '../../assets/Group 21 (1).png';
 import styles from './Navbar.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
+      <div className={styles.links}>
+        <NavLink exact activeClassName={styles.active_class} to="/">
+          Home
+        </NavLink>
+        <NavLink exact activeClassName={styles.active_class} to="/about">
+          About Us
+        </NavLink>
+        <NavLink exact activeClassName={styles.active_class} to="/sports">
+          Sports
+        </NavLink>
+        <NavLink exact activeClassName={styles.active_class} to="/gallery">
+          Gallery
+        </NavLink>
+        <NavLink exact activeClassName={styles.active_class} to="/contact">
+          Contact
+        </NavLink>
+        <NavLink exact activeClassName={styles.passive_class} to="/">
+          Registrations
+        </NavLink>
+      </div>
       <img src={Urja_logo} alt="URJA LOGO" className={styles.logo} />
       
       <div className={styles.annualTechFest}>
@@ -21,7 +40,7 @@ const Navbar = () => {
       </div>
       <div className={styles.alongTiger1}> 
         <img src={Roar} alt="" />
-        </div>
+      </div>
     </div>
   );
 }
