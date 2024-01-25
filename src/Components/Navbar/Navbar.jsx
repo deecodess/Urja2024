f072463f5a16795bd7f0735e2e924fa763696692
Deepanshi Sharma <90607584+deecodess@users.noreply.github.com>
2024-01-25 11:17:52 +0530

    Navbar Updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React ,{useState}from 'react';
 import styles from './Navbar.module.css';
 import { NavLink } from 'react-router-dom';
 import Urja_logo from '../../assets/Group 23.png';
-import thapar_logo from '../../assets/thapar 1.png'
+import Thapar_Logo from '../../assets/thapar logo.png';
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
@@ -12,8 +12,10 @@ const Navbar = () => {
   };
   return (
     <nav>
+    
      <div className={styles.links + (showLinks ? ' show' : '')}>
         <img src={Urja_logo} alt="URJA LOGO" className={styles.logo} />
+        <img src={Thapar_Logo} alt="THAPAR LOGO" className={styles.logo2} />
         <div className={styles['hamburger-icon']} onClick={toggleLinks}>
           ☰
         </div>
