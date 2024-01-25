6213f842f369ab9a427d76421b2532e6ab8bde44
Deepanshi Sharma <90607584+deecodess@users.noreply.github.com>
2024-01-25 11:46:02 +0530

    Navbar added in team

--- src/Pages/Team/Team.jsx
+++ src/Pages/Team/Team.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import styles from './Team.module.css';
+import Navbar from '../../Components/Navbar/Navbar';
 
 const Team = () => {
   return (
+    <Fragment>
+      <Navbar></Navbar>
     <div className={styles.container}>
       <h1>Coming Soon!</h1>
     </div>
+    </Fragment>
   )
 }
 
