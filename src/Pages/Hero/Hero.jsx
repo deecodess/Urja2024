3cbea993c324bb8db27457f819fd478541f6a3ca
Kumar Shresth <kumarshresth2004@gmail.com>
2024-01-27 22:58:13 +0530

    new hero page

--- src/Pages/Hero/Hero.jsx
+++ src/Pages/Hero/Hero.jsx
@@ -5,6 +5,8 @@ import styles from "./Hero.module.css";
 import Navbar from "../../Components/Navbar/Navbar";
 import bg2 from "../../assets/diyaAsset 1 1.png";
 import background from "../../assets/background.png";
+import background2 from "../../assets/background2.jpg"
+import background3 from "../../assets/background3.jpg"
 import About from "../About/About.jsx";
 import Events from "../Events/Events.jsx";
 import Gallery from "../Gallery/Gallery.jsx";
@@ -13,6 +15,7 @@ import Contact from "../Contact/Contact.jsx";
 import urja_aftermovie_final from "../../assets/urja_aftermovie_final.mp4";
 import logo from "../../assets/logo.jpeg";
 import phone from "../../assets/phone.png";
+import New_Logo from "../../assets/new_lion.png"
 
 import download from './../../assets/download.png';
 import app from './../../assets/app.png';
@@ -24,7 +27,8 @@ const Hero = () => {
 
       <div className={styles.parent}>
       <div className={styles.container}>
-        <img src={background} className={styles.background} alt="" />
+        <img src={background2} className={styles.background} alt="" />
+        
         <div className={styles.bg1}>
         <img src={bg2} className={styles.bg2} alt="" />
        </div>
@@ -42,7 +46,7 @@ const Hero = () => {
         <h1 className={styles.mobileonlytext}>THE ROAR OF EMINENCE</h1>
         <img src={Main} alt="" className={styles.mainImage} />
           <h1 className={styles.firstText}> THE ROAR</h1>
-          <img src={Lion_img} alt="" className={styles.lionImage} />
+          <img src={New_Logo} alt="" className={styles.lionImage} />
           <h1 className={styles.secondText}> OF EMINENCE</h1>
         </div>
         <div className={styles.button}>
