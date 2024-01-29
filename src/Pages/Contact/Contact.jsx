2040321a65b2682a70290944f5091627b251e140
tusharsinghal17 <100711332+tusharsinghal17@users.noreply.github.com>
2024-01-29 14:29:45 +0530

    map added and mobile view css fixed

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -51,6 +51,9 @@ const ContactUs = () => {
                 <img src={insta} alt="" />
               </a>
             </div>
+            <div className={styles['map']}>
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3442.8072010873466!2d76.36212627502266!3d30.356428803722675!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391028ab86533db5%3A0x93cc1f72eae1c9a8!2sThapar%20Institute%20of%20Engineering%20%26%20Technology!5e0!3m2!1sen!2sin!4v1706517068574!5m2!1sen!2sin" target="blank"></iframe>
+            </div>
           </div>
           <div className={styles['Big-logo']}>
             <img src={lionImage} alt="" id={styles['BigImage']} />
